76344f56fc675f2fba059e56b064dd20a6b035db
Kirill <kornilov.kd@mail.ru>
2025-02-20 18:45:41 +0300

    third script

---
+++ training/3_ping_servers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" != "" ]; then
+	server_list="$1"
+else
+	echo -e "\nНе передан файл со списком хостов.\n"
+	exit 1
+fi
+
+if [ ! -f "$server_list" ]; then
+	echo -e "Файл $server_list не существует."
+	exit 1
+fi
+
+while IFS= read -r server; do
+	if ping -c 1 -W 1 "$server" &> /dev/null; then
+		echo -e "Сервер $server доступен."
+	else
+		echo -e "Сервер $server недоступен."
+	fi
+done < "$server_list"
